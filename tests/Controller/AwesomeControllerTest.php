9dbd147da25c1897ce09c826b02d2b8c99e5e1ab
gbaguidih <gbaguidihillary039@gmail.com>
2025-01-22 21:39:21 +0100

    Deuxième commit

---
+++ tests/Controller/AwesomeControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+final class AwesomeControllerTest extends WebTestCase{
+    public function testIndex(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/awesome');
+
+        self::assertResponseIsSuccessful();
+    }
+}
